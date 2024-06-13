f23abcb7329060788fc2452348f8b4b37825b1df
ruslanrahim67 <ruslanrahim67@gmail.com>
2024-06-13 11:27:45 +0400

    responsivlik ve filter son deyisiklikler

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -4,3 +4,10 @@ function toggleNavbar() {
     const dropdownUl=document.querySelector(".navbar-dropdown");
     dropdownUl.classList.toggle('show');
 }
+
+
+function toggleAside() {
+    const asideBurger=document.querySelector("aside");
+    asideBurger.classList.toggle('show2');
+}
+
